211778c617ad9f1777101366ee834d677b1dcf3f
sarten8 <sarten8@gmail.com>
2019-08-09 06:33:17 -0300

    commit arreglo deploy 2

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import Layout from '../components/MyLayout'
 import Home from './home'
 import About from './about'
@@ -6,6 +7,16 @@ import Works from './works'
 export default function Index() {
   return (
     <Layout>
+      <Head>
+        <meta charset="utf-8" />
+        <link rel="shortcut icon" href="/static/triangle.png" />
+        <title>b.</title>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1"
+          key="viewport"
+        />
+      </Head>
       <Home />
       <About />
       <Works />
