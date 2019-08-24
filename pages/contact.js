bc407078336af1c79da9521bbd7fd846141ba5a0
sarten8 <sarten8@gmail.com>
2019-08-24 20:22:47 -0300

    fix 2

--- pages/contact.js
+++ pages/contact.js
@@ -3,34 +3,20 @@ import css from '../styles/styles.css'
 export default () => (
   <section className={css.contact} id="contact">
     <div className={css.imageBackground} />
-    <div>
-      <p>::: contacto</p>
-    </div>
-    <div className={css.social}>
-      <a href="https://www.instagram.com/suckthistangerine/" target="_blank">
-        <div>
-          <img src="/static/icons/instagram.png" alt="" />
-          <span>instagram</span>
-        </div>
-      </a>
-      <a
-        href="mailto:bernardafz@gmail.com"
-        target="_blank"
-      >
-        <div>
-          <img src="/static/icons/gmail.png" alt="" />
-          <span>gmail</span>
-        </div>
-      </a>
-      <a
-        href="https://uy.linkedin.com/in/bernarda-fern%C3%A1ndez-r%C3%ADos-5139537a"
-        target="_blank"
-      >
-        <div>
-          <img src="/static/icons/linkedin.png" alt="" />
-          <span>linkedIn</span>
-        </div>
-      </a>
-    </div>
+    <a href="https://www.instagram.com/suckthistangerine/" target="_blank">
+      <img src="/static/icons/instagram.png" alt="" />
+      <span>instagram</span>
+    </a>
+    <a href="mailto:bernardafz@gmail.com" target="_blank">
+      <img src="/static/icons/gmail.png" alt="" />
+      <span>gmail</span>
+    </a>
+    <a
+      href="https://uy.linkedin.com/in/bernarda-fern%C3%A1ndez-r%C3%ADos-5139537a"
+      target="_blank"
+    >
+      <img src="/static/icons/linkedin.png" alt="" />
+      <span>linkedIn</span>
+    </a>
   </section>
 )
